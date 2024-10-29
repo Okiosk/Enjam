ec58986d034e867be11892cd0042983f6774d802
Okiosk <victorouzaud@gmail.com>
2024-10-29 14:54:53 +0100

    art

---
+++ Assets/candy_box_script.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+public class candy_box_script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("qdhsdg");
+    }
+}
