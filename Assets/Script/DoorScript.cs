c394ad7d8b356c2f6e8dfe968747ffa363427900
Julien <julien.pr86@gmail.com>
2024-10-29 15:39:32 +0100

    player and door action update

--- Assets/Script/DoorScript.cs
+++ Assets/Script/DoorScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    private bool playerOnDoor = false;
     private bool isOpen = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,15 +13,28 @@ public class DoorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerOnDoor && Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeDoorState();
+        }
     }
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.name == "Player")
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                isOpen = !isOpen;
-                Debug.Log("Door open is "+isOpen.ToString());
-            }
+       if (collision.name == "Player")
+       {
+            playerOnDoor = true;
+       }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       if (collision.name == "Player")
+       {
+            playerOnDoor = false;
+       }
+    }
+    private void ChangeDoorState()
+    {
+        isOpen = !isOpen;
+        Debug.Log("Door open is "+isOpen.ToString());
     }
 }
