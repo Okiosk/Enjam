2cb66ac72022d66002da74b636874f4b238eeaca
Julien <julien.pr86@gmail.com>
2024-10-29 16:58:01 +0100

    tag player and add player in door

--- Assets/Script/DoorScript.cs
+++ Assets/Script/DoorScript.cs
@@ -5,9 +5,11 @@ public class DoorScript : MonoBehaviour
     private bool playerOnDoor = false;
     private bool isOpen = false;
     public ChildScript child;
+    public PlayerScript player;
     void Start()
     {
         child = GameObject.FindGameObjectWithTag("Child").GetComponent<ChildScript>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
     }
 
     void Update()
