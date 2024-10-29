274c0326c4e6cb1acbd1353eff7614e33b832619
Okiosk <victorouzaud@gmail.com>
2024-10-29 15:23:26 +0100

    box update 2

--- Assets/candy_box_script.cs
+++ Assets/candy_box_script.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 public class candy_box_script : MonoBehaviour
 {
+    private bool playerIn = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,14 +12,20 @@ public class candy_box_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerIn) 
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("qdhsdg");
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("sdbdfbwdfvvvvvvvvvvvvvvvvvvvvvvvvvvvbdq");
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("qdhsdg");
-        }
+        playerIn = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerIn = false;
     }
 }
