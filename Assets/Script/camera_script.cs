7e1e0f7e18b0af0e6c3598d4043e369c443886ae
Okiosk <victorouzaud@gmail.com>
2024-10-30 10:47:11 +0100

    camera avance

--- Assets/Script/camera_script.cs
+++ Assets/Script/camera_script.cs
@@ -1,16 +1,52 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.Rendering;
+using System.Numerics;
 
 public class camera_script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float zoom;
+    private float zoomMultiplier = 4f;
+    private float minZoom = 2f;
+    private float maxZoom = 8f;
+    private float velocity = 0f;
+    private float smoothTime = 0.001f;
+
+    private bool zoomEnCour = false;
+    private bool dezoomEnCour = false;
+    private UnityEngine.Vector3 target;
+    private float speed = 0.001f;
+
+    [SerializeField] private Camera cam;
+
+    private void Start()
     {
-        
+        zoom = cam.orthographicSize;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (zoomEnCour)
+        {
+            float scroll = 1;
+            zoom -= scroll * zoomMultiplier;
+            zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+            cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+            gameObject.transform.position = UnityEngine.Vector3.MoveTowards(transform.position, target, speed);
+            if (cam.orthographicSize > maxZoom)
+            {
+                zoomEnCour = false;
+            }
+        }
+    }
+
+    public void doZoom(float x, float y)
+    {
+        zoomEnCour = true;
+        target.x = x;
+        target.y = y;
+        target.z = -10;
+
     }
 }
--- Assets/Script/candy_box_script.cs
+++ Assets/Script/candy_box_script.cs
@@ -8,6 +8,7 @@ public class candy_box_script : MonoBehaviour
     public string typeCandy;
     private ui_le_script uiScript;
     private PlayerScript pScript;
+    private camera_script camera;
 
     private bool playerIn = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,6 +16,8 @@ public class candy_box_script : MonoBehaviour
     {
         uiScript = GameObject.FindGameObjectWithTag("UI").GetComponent<ui_le_script>();
         pScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<camera_script>();
+
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class candy_box_script : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space) && (pScript.candyCarry == "none" || typeCandy == "none"))
             {
+                camera.doZoom(gameObject.transform.position.x, gameObject.transform.position.y);
                 pScript.candyCarry = typeCandy;
                 uiScript.changeIcon(typeCandy);
             }
