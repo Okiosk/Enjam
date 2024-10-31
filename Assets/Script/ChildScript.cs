a4f6c8eb26a2852eaf2293d6004d55f6d060c81e
Julien <julien.pr86@gmail.com>
2024-10-31 09:22:15 +0100

    timer change

--- Assets/Script/ChildScript.cs
+++ Assets/Script/ChildScript.cs
@@ -7,7 +7,7 @@ public class ChildScript : MonoBehaviour
 {
     private int minTime = 10, maxTime = 10;
     private float childTimer = 0;
-    private int waitingTime = 10;
+    private int waitingTime = 12;
     private float childWaitingTimer;
     public bool childIsWaiting = false;
     private string[] colors = {"orange","green","blue","pink"};
