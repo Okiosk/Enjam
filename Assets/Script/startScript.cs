cd3115279249026ce865e78d1a9194eea4b7cceb
Okiosk <victorouzaud@gmail.com>
2024-10-31 01:37:22 +0100

    start avance

--- Assets/Script/startScript.cs
+++ Assets/Script/startScript.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SearchService;
 
 public class startScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        Debug.Log("sdsdsdsdsdsdsdsdsdsdsdsd");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape)) 
+        {
+            Debug.Log("qrrgzesg");
+            SceneManager.LoadScene("Scenes/SampleScene");
+        }
+
+    }
+    public void change()
+    {
+
+        SceneManager.LoadScene("Scenes/SampleScene");
     }
 }
